2f4cec1976711280b282e40735a7bd8a00879537
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-10 11:16:54 -0500

    BookForm added, index.css modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BookContextProdvider from './context/BookContext';
 import NavBar from './components/Navbar';
 import BookList from './components/BookList';
+import NewBookForm from './components/BookForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BookContextProdvider>
         <NavBar />
         <BookList />
+        <NewBookForm />
       </BookContextProdvider>
     </div>
   );
