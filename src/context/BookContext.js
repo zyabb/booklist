8fc0cac26b76560a4c077f55e9d786932a33fbc3
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-16 15:51:32 -0500

    LocalStorage added

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useState, useReducer } from 'react';
+import React, { createContext, useState, useReducer, useEffect } from 'react';
 import uuid from 'uuid/v1';
-import { bookReducer } from '../reducer/bookReducer';
+import { bookReducer } from '../reducer/BookReducer';
 export const BookContext = createContext();
 
 const BookContextProdvider = props => {
@@ -9,11 +9,21 @@ const BookContextProdvider = props => {
   //   { title: 'Second Book', author: 'Selena Gomez', id: 2 },
   //   { title: 'Third Book', author: 'Katy Perry', id: 3 }
   // ]);
-  const [books, dispatch] = useReducer(bookReducer, [
-    { title: 'First Book', author: 'Will Smith', id: 1 },
-    { title: 'Second Book', author: 'Selena Gomez', id: 2 },
-    { title: 'Third Book', author: 'Katy Perry', id: 3 }
-  ]);
+  // const [books, dispatch] = useReducer(bookReducer, [
+  //   { title: 'First Book', author: 'Will Smith', id: 1 },
+  //   { title: 'Second Book', author: 'Selena Gomez', id: 2 },
+  //   { title: 'Third Book', author: 'Katy Perry', id: 3 }
+  // ]);
+
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
+
   // const addBook = (title, author) => {
   //   setBooks([...books, { title, author, id: uuid() }]);
   // };
